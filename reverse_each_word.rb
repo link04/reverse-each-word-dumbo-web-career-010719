662946241487f929a0c4e86cbac1622ba9848a94
Maximo Bautista <max.bautista97@gmail.com>
2018-12-25 16:07:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(string)
   array_of_string = string.split
+  arra_reversed = []
   array_of_string.each do |word|
-    puts word.reverse
+     arra_reversed << word.reverse
   end
 end
